61eaedf73f81aebf984cba61b55d9173bf1041b8
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-03-21 11:25:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+ document.getElementById('userForm').addEventListener('submit', function(event) {
+	 event.preventDefault();
+	const age = parseInt(document.getElementById('age').value);
+	const name = document.getElementById('name').value;
+	 
+      if (!name || !age) {
+                alert('Please fill in all fields.');
+                return;
+      }
+	 
+	const prom = new Promise((resolve,reject) =>{
+		setTimeout(() => {
+			if(age >= 18){
+				resolve(`Welcome, ${name}. You can vote`);
+			}else{
+			reject(`Oh sorry ${name}. You aren't old enough`);
+		     }
+		},4000);
+		
+	});
+
+	 prom.then((data) =>{
+	     alert(data);
+	}).catch((err) => {
+		alert(err);
+	});
+});
+
+
